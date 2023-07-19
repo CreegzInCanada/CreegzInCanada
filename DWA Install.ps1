18aa27811db09717e63c88687a8d1f0e265c5b46
CreegzInCanada <63323026+CreegzInCanada@users.noreply.github.com>
2023-07-19 09:57:39 -0700

    Update DWA Install.ps1

--- DWA Install.ps1
+++ DWA Install.ps1
@@ -7,4 +7,5 @@ else
     Write-Host "Folder Created successfully"
 }
 Invoke-WebRequest https://cf-dl.datto.com/dwa/DattoWindowsAgent.exe -OutFile C:\Installers\DattoWindowsAgent.exe
-C:\Installers\DattoWindowsAgent.exe /quiet /norestart=
+C:\Installers\DattoWindowsAgent.exe /quiet /norestart
+<# Remove /norestart from the above command to force a reboot immediately after the installation completes, which is very fast. #>
